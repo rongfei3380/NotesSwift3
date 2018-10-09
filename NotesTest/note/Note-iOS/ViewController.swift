95675d1789165718fae8c99cec504bbdf4ed9b47
rongfei3380 <chengfeir@staff.l99.com>
2018-10-09 10:44:10 +0800

    new:  new  target

---
+++ NotesTest/note/Note-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Note-iOS
+//
+//  Created by chengfei on 2018/9/20.
+//  Copyright © 2018 chengfeir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
